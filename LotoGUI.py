c715d3f02a318cf4f0e837382f134d5cadab1596
gersonpas <sobrinho.gerson@gmail.com>
2021-05-01 01:41:35 -0300

    Versão 2.1 => Loterias da CEF com apresentação Gráfica.

--- LotoGUI.py
+++ LotoGUI.py
@@ -1,21 +1,16 @@
 import PySimpleGUI as sg
 from random import randint, sample,choice
-
 sg.theme('DarkAmber')  # Add a touch of color
 layout = [[sg.Text('Faça sua Escolha:')],
           [sg.Text('- QUINA\n- MEGA SENA\n- DUPLA SENA\n'
                    '- LOTOFÁCIL\n- LOTOMANIA\n- DIA DE SORTE')],
           [sg.Text('Escolha sua Loteria e Click OK para iniciar o Programa'), sg.InputOptionMenu(['QUINA', 'MEGA SENA', 'DUPLA SENA', 'LOTOFÁCIL', 'LOTOMANIA', 'DIA DE SORTE'])],
           [sg.Button('Ok'), sg.Button('Sair')]]
-
 window = sg.Window('Números da Sorte - Jogos da CEF', layout)
-
-
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Sair':  # Fecha janela se usuario clicar sair ou fechar janela.
         break
-
 if values[0] == 'QUINA':
     opção, init, fim, qdezInit, qdezFim = 'QUINA', 0, 80, 5, 15
 if values[0] == 'MEGA SENA':
@@ -32,10 +27,6 @@ if values[0] == 'DIA DE SORTE':
 Meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro',
              'Novembro', 'Dezembro']
 mesecolhido = choice(Meses) # escolhe um mês dentre os meses da lista "Meses".
-
-
-#qdezenas = int(input(f'\033[0;33mEscolha um jogo entre {qdezInit} e {qdezFim} Dezenas\033[0;33m: '))
-
 #window.close()
 
 sg.theme('DarkTeal9')    #DarkAmber
